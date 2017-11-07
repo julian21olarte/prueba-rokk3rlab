43d5cb1b27e8d6feeefc9ad021b30339ca0c1b5e
julian <julianolarte@kubesoft.com>
2017-11-07 17:39:28 -0500

    mas cambios

--- app.js
+++ app.js
@@ -45,13 +45,24 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-app.use( (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-Key, Origin, X-Request-With, Content-type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET. POST, OPTIONS, PUT, DELETE');
-  next();
-});
+app.use(function (req, res, next) {
+  
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+  });
 
 // error handler
 app.use(function(err, req, res, next) {
