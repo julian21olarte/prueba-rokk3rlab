05f5329de45adf003e2eee89e35cf6c0843ae3f2
julian <julianolarte@kubesoft.com>
2017-11-07 17:37:14 -0500

    mas cambios

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(function(req, res, next) {
 app.use( (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-Key, Origin, X-Request-With, Content-type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET. POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET. POST, OPTIONS, PUT, DELETE');
   next();
 });
