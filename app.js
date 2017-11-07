e4ce52874e7c00af9f5e654fb2325559b4ece9a5
julian <julianolarte@kubesoft.com>
2017-11-07 17:54:47 -0500

    cambios

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var cors = require('cors');
 
 var index = require('./routes/index');
 var task = require('./routes/task');
@@ -46,7 +45,24 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-app.use(cors());
+app.use(function (req, res, next) {
+  
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+  });
 
 // error handler
 app.use(function(err, req, res, next) {
